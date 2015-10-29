c6c93eb8cebd69cff2efb36bce79fd7524aa3c39
nikolasbeckel <nikolasbeckel@hotmail.de>
2015-10-29 12:27:00 +0100

    Javascript Aufgaben gemacht (BASIC)

--- javascript-training/mi-onepager-training/assets/js/dozentenauflistung.js
+++ javascript-training/mi-onepager-training/assets/js/dozentenauflistung.js
@@ -5,13 +5,13 @@
 
 /*
  * TODOs sind in den folgenden Zeilen zu finden:
- *  - Zeile 31    * Element über eine ID selektieren/holen
- *  - Zeile 119   * 'dl'-Element erzeugen
- *  - Zeile 158   * Fehlerabfrage mittels einer if-Abfrage
- *  - Zeile 192   * 'figure'-Element erzeugen
- *  - Zeile 215   * 'figcaption'-Element erzeugen
+ *  - Zeile 31    * Element über eine ID selektieren/holen (done)
+ *  - Zeile 119   * 'dl'-Element erzeugen (done)
+ *  - Zeile 158   * Fehlerabfrage mittels einer if-Abfrage (done)
+ *  - Zeile 192   * 'figure'-Element erzeugen (done)
+ *  - Zeile 215   * 'figcaption'-Element erzeugen (done)
  *  - Zeile 227   * Erzeugung eines JavaScript-Objekts mittels der impliziten Kurzschreibweise
- *  - Zeile 256   * Vier Syntaxfehler beheben
+ *  - Zeile 256   * Vier Syntaxfehler beheben (done)
  */
 
 
@@ -33,7 +33,8 @@ var lecturers_filepath  = "https://raw.githubusercontent.com/th-koeln/" +
      *  der ID 'dozenten' geholt werden
      * ##
      */
-var dozenten_section    = /* HIER */ ;
+
+var dozenten_section    = document.getElementById('dozenten');
 var loading_element     = dozenten_section.querySelector( '.dozenten_ladehinweis' );
 
 
@@ -120,7 +121,7 @@ function construct_dl( info ) {
      *  Ein 'dl'-Element erzeugen
      * ##
      */
-    var dl_element = /* HIER */ ;
+    var dl_element = document.createElement('dl');
 
     /* Durch alle Informationseinträge traversieren */
     for( var key in info ) {
@@ -160,7 +161,7 @@ getJSON( lecturers_filepath, function( err, data ) {
      *  dann soll der Vorgang abgebrochen werden
      * ##
      */
-    if( /* Hier */ ) {
+    if( err != null ) {
         error_output( err );
         return;
     }
@@ -193,7 +194,7 @@ getJSON( lecturers_filepath, function( err, data ) {
          *  Ein 'figure'-Element erzeugen
          * ##
          */
-        var figure_element = /* HIER */;
+        var figure_element = document.createElement('figure');
 
         /* Basisinformationen vorbereiten */
         var basic_infos = value.basic_infos || {};
@@ -216,7 +217,7 @@ getJSON( lecturers_filepath, function( err, data ) {
          *  Ein 'figcaption'-Element erzeugen
          * ##
          */
-        var figcaption_element = /* HIER */;
+        var figcaption_element = document.createElement('figcaption');
 
         var name_headline_element = document.createElement( 'h1' );
         name_headline_element.innerHTML = lecturer_name;
@@ -230,13 +231,12 @@ getJSON( lecturers_filepath, function( err, data ) {
          *  Nutzen Sie die gegebenen Schlüssel-Wert-Paare
          * ##
          */
-        var basic_infos =
-          /*
+        var basic_infos = {
            'Raum:'   : basic_infos.room,
            'Telefon:': basic_infos.phone_number,
            'eMail:'  : lecturer_email,
            'Website:': basic_infos.website
-           */
+           }
 
         var dl_element_basic = construct_dl( basic_infos );
 
@@ -258,7 +258,7 @@ getJSON( lecturers_filepath, function( err, data ) {
          *  Beheben Sie diese.
          * ##
          */
-        FOR( key in custom_infos ) { /* Beginn der for-Schleife */
+        for ( var key in custom_infos ) { /* Beginn der for-Schleife */
             var value = custom_infos[ key ];
 
             /* Leere Einträge überspringen */
@@ -266,7 +266,8 @@ getJSON( lecturers_filepath, function( err, data ) {
               return;
             }
 
-            prepared_custom_infos[ value.title ] = value.content
+            prepared_custom_infos[ value.title ] = value.content;
+        }
         /* Ende der for-Schleife */
 
         var dl_element_custom = construct_dl( prepared_custom_infos );
